62af1d87bf9662b3244decb630302d54f691ff35
Darrow Palast <dpalast@gmail.com>
2019-08-03 18:57:26 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,19 @@
 def consolidate_cart(cart)    #this cart is an array of hashes
   consolidated = { }
   cart.each do |(key, value)|
-    if consolidated.has_key?(key)
-      consolidated[value][count] += 1
-    else
-      consolidated[key] = value
-      consolidated[value][count] = 1
-    end
+    
   end
-  consolidated
 end
+    
+    
+   
+   
+   
+   
+   
+   
+   
+   
 
 def apply_coupons(cart, coupons)
   # code here
