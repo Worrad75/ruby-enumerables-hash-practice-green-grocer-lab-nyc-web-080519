3c9307184c1178a4aa895114a52cd6db5d838641
Darrow Palast <dpalast@gmail.com>
2019-08-03 19:43:25 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -12,28 +12,53 @@ def consolidate_cart(cart)    #this cart is an array of hashes
 end
     
 
-def apply_coupons(cart, coupons)
+# def apply_coupons(cart, coupons)
   
-  coupons.each do |coupon_hash|
-    coupon_hash.each do |att, val|
-      name = coupon_hash[:item]
+#   coupons.each do |coupon_hash|
+#     coupon_hash.each do |att, val|
+#       name = coupon_hash[:item]
       
-      if cart[name] && cart[name][:count] >= coupon_hash[:num]
-        if cart["#{name} W/COUPON"]
-          cart["#{name} W/COUPON"][:count] += 1
-        else
-          cart["#{name} W/COUPON"] = {:price=>coupon_hash[:cost],
-          :clearence=>cart[name][:clearance], :count=>1}
-        end
+#       if cart[name] && cart[name][:count] >= coupon_hash[:num]
+#         if cart["#{name} W/COUPON"]
+#           cart["#{name} W/COUPON"][:count] += 1
+#         else
+#           cart["#{name} W/COUPON"] = {:price=>coupon_hash[:cost],
+#           :clearence=>cart[name][:clearance], :count=>1}
+#         end
         
-        cart[name][:count] -= coupon_hash[:num]
+#         cart[name][:count] -= coupon_hash[:num]
+#       end
+#     end
+#   end
+#   cart
+# end
+
+def apply_coupons(cart, coupons)
+  hash = cart
+  coupons.each do |coupon_hash|
+    # add coupon to cart
+    item = coupon_hash[:item]
+
+    if !hash[item].nil? && hash[item][:count] >= coupon_hash[:num]
+      temp = {"#{item} W/COUPON" => {
+        :price => coupon_hash[:cost],
+        :clearance => hash[item][:clearance],
+        :count => 1
+        }
+      }
+      
+      if hash["#{item} W/COUPON"].nil?
+        hash.merge!(temp)
+      else
+        hash["#{item} W/COUPON"][:count] += 1
+        #hash["#{item} W/COUPON"][:price] += coupon_hash[:cost]
       end
+      
+      hash[item][:count] -= coupon_hash[:num]
     end
   end
-  cart
+  hash
 end
-
-
 
 
 
