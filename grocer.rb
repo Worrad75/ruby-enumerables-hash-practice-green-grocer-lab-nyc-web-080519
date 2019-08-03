7fa269c14757847768b8e9a7b1eaaaf841523259
Darrow Palast <dpalast@gmail.com>
2019-08-03 21:08:29 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -37,7 +37,7 @@ end
 def apply_clearance(cart)
   cart.each do |name, item_hash|
     if item_hash[:clearance] == true
-      (item_hash[:price]*0.8).round(2) 
+      item_hash = (item_hash[:price]*0.8).round(2)
     end
   end
   cart
