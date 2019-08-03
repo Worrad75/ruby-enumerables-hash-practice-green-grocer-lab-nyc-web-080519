09cdd8fcbf9baab4b808513a740ead32fd594a2c
Darrow Palast <dpalast@gmail.com>
2019-08-03 21:06:49 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -41,6 +41,23 @@ def apply_clearance(cart)
   cart
 end
 
+
+# def apply_clearance(cart) 
+#   cart.each do |item, attribute_hash| 
+#     if attribute_hash[:clearance] == true 
+#       attribute_hash[:price] = (attribute_hash[:price] *
+#       0.8).round(2) 
+#     end 
+#   end 
+# cart 
+# end
+
+
+
+
+
+
+
 def checkout(cart, coupons)
   # code here
 end
