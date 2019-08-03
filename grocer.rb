89b3f136aaf726784a932c7dad991aafd0fef581
Darrow Palast <dpalast@gmail.com>
2019-08-03 19:03:21 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,15 @@
 def consolidate_cart(cart)    #this cart is an array of hashes
-  organized_cart = {}
+  compact_cart = {}
   
   cart.each do |element|
-    count = 0
+    # count = 0
     element.each do |item, hash|
-      organized_cart[item] ||= hash
-      organized_cart[item][:count] ||= 0
-      organized_cart[item][:count] += 1
+      compact_cart[item] ||= hash
+      compact_cart[item][:count] ||= 0
+      compact_cart[item][:count] += 1
     end
   end
-  return organized_cart
+  compact_cart
 end
     
     
